bd73af3e223340c2a5ec8c4f37ccd38397ecf2e8
SeungHo Han <furtherdream.ceo@gmail.com>
2019-02-19 17:17:49 +0900

    prttier 사용

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,11 +1,11 @@
 // Query는 database에게는 문제 같은 거임.
 // 그래서 이 쿼리를 어떠한 방식으로 해결해야 함
-import { people } from './db'
+import { people } from "./db"
 
 const resolvers = {
-    Query : {
-        people: () => people
-    }
+  Query: {
+    people: () => people
+  }
 }
 
-export default resolvers;
+export default resolvers
