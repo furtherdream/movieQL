53b86920c36bfaf3fa4f234d6b40d7d5fb4a10a0
SeungHo Han <furtherdream.ceo@gmail.com>
2019-02-19 17:09:42 +0900

    #6 Extending the Schema part Two

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,10 @@
 // Query는 database에게는 문제 같은 거임.
 // 그래서 이 쿼리를 어떠한 방식으로 해결해야 함
-const nicolas = {
-    name : "Nicolas",
-    age : 20,
-    gender : "female"
-}
+import { people } from './db'
 
 const resolvers = {
     Query : {
-        person: () => nicolas
+        people: () => people
     }
 }
 
