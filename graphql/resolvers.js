7984e917d04e819073f2e509ed3d5fb306600d2f
SeungHo Han <furtherdream.ceo@gmail.com>
2019-02-19 17:59:39 +0900

    #7 Creating Queries with Arguments

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,13 @@
 // Query는 database에게는 문제 같은 거임.
 // 그래서 이 쿼리를 어떠한 방식으로 해결해야 함
-import { people } from "./db"
+import { people, getById } from "./db"
 
 const resolvers = {
   Query: {
-    people: () => people
+    people: () => people,
+    person: (_, { id }) => getById(id)
+    // 처음 건 현재 오브젝트를 보내는 오브젝트, 두 번째는 argument
+    // console.log(args)로 찍히는 값이 { id: 1 } 이었음
   }
 }
 
