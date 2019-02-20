c213c68e3587e74f5382ea6ea64f9eaf3ec171c3
SeungHo Han <furtherdream.ceo@gmail.com>
2019-02-20 17:07:30 +0900

    #13 getMovies and getMovie. Axios, Async

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,11 @@
 // Query는 database에게는 문제 같은 거임.
 // 그래서 이 쿼리를 어떠한 방식으로 해결해야 함
-import { getMovies } from "./db"
+import { getMovies, getMovie } from "./db"
 
 const resolvers = {
   Query: {
-    movies: (_, { limit, rating }) => getMovies(limit, rating)
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id)
   }
 }
 
