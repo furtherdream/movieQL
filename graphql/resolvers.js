4dbb4818dcb670c93bd734d5620c3d1a4895caec
SeungHo Han <furtherdream.ceo@gmail.com>
2019-02-20 17:25:53 +0900

    #14 movie Suggestions

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,11 +1,12 @@
 // Query는 database에게는 문제 같은 거임.
 // 그래서 이 쿼리를 어떠한 방식으로 해결해야 함
-import { getMovies, getMovie } from "./db"
+import { getMovies, getMovie, getSuggestions } from "./db"
 
 const resolvers = {
   Query: {
-    movies: (_, { limit, rating }) => getMovies(limit, rating),
-    movie: (_, { id }) => getMovie(id)
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
   }
 }
 
